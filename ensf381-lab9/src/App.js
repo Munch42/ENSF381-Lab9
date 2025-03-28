ac59c50a65030660e56301f72b2e8e6c78e431b2
Munch42 <41879159+Munch42@users.noreply.github.com>
2025-03-28 11:24:49 -0600

    Added Login component

--- ensf381-lab9/src/App.js
+++ ensf381-lab9/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HousePricePredictor from './HousePricePredictor';
+import Login from './Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={} />
+        <Route path="/" element={<Login />} />
         <Route path="predict/" element={<HousePricePredictor />} />
       </Routes>
     </BrowserRouter>
