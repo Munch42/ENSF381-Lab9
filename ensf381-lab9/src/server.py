710c4ce12e667ce071b4fe4b78d502c2bae4790c
Munch42 <41879159+Munch42@users.noreply.github.com>
2025-03-28 12:24:05 -0600

    Added prediction to server

--- ensf381-lab9/src/server.py
+++ ensf381-lab9/src/server.py
@@ -1,22 +1,45 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
 
 # Assigning multiple routes to the same function
-@app.route('/',methods=['POST'])
-@app.route('/main',methods=['POST'])
-def home():
-  data = request.get_json() # Retrieves the JSON data from the incoming request and store it in a dictionary
-  print(data['username']) # Accesses the 'username' field from the data dictionary
+@app.route('/predict_house_price',methods=['POST'])
+def predict_house_price():
+  model = joblib.load("./src/random_forest_model.pkl")
+  data = request.json
 
-  print('This is the home API')
+  cats = True if 'pets' in data and data['pets'] else False
+  dogs = True if 'pets' in data and data['pets'] else False
 
-  #return 'Welcome to the home page!'
-  #return 'Welcome {} to the home page!'.format(data['username'])
-  msg = 'Welcome {} to the home page!'.format(data['username'])
-  return {'msg':msg}
+  sample_data = [
+    data['city'],
+    data['province'],
+    float(data['latitude']),
+    float(data['longitude']),
+    data['lease_term'],
+    data['type'],
+    float(data['beds']),
+    float(data['baths']),
+    float(data['sq_feet']),
+    data['furnishing'],
+    data['smoking'],
+    cats,
+    dogs
+  ]
+
+  sample_df = pd.DataFrame([sample_data], columns=[
+    'city', 'province', 'latitude', 'longitude', 'lease_term',
+    'type', 'beds', 'baths', 'sq_feet', 'furnishing',
+    'smoking', 'cats', 'dogs'
+  ])
+
+  predicted_price = model.predict(sample_df)
+
+  return jsonify({"predicted_price": float(predicted_price[0])})
 
 @app.route('/about')
 def about():
